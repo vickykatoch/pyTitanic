9bf6b9571d10f5d8e64f466deac7fc524cacd53e
Balwinder Katoch <balwinderkatoch@MacBook-Pro.local>
2020-02-03 19:08:42 -0500

    changes

--- src/scratch/plot.py
+++ src/scratch/plot.py
@@ -3,9 +3,7 @@ from matplotlib import pyplot as plt
 from os import path
 
 
-# file_name = path.join(path.curdir,'../../data/external/countries.csv')
-print(path.curdir)
-file_name = '..\\..\\data\\external\\countries.csv'
-# pd.read_csv(file_name)
-print(file_name)
-print(path.exists(file_name))
+file_name = path.join(path.curdir,'../../data/external/countries.csv')
+df = pd.read_csv(file_name)
+plt.plot(df.country, df.lifeExpectancy)
+plt.show()
